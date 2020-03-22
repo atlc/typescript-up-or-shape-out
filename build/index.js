751f0b2fa9c1c934fd973ceb1553aa8846ba9e37
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-03-22 04:31:33 -0500

    Discovered JSDocs/typedoc and implemented thorough comment definitions

--- build/index.js
+++ build/index.js
@@ -11,23 +11,52 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+/**
+ * Definitions of the generic parent shape class.
+ */
 var Shape = /** @class */ (function () {
-    function Shape(name, width, height, radius) {
+    /**
+     * Assigns generic Shape attributes.
+     * @param name Only required parameter. Supplied in the constructor of each child constructor.
+     * @param width Considered optional, though all current shapes will return this (including circles).
+     * @param height Considered optional, though all current shapes will return this (including circles).
+     * @param radius Considered optional, all constructors but the circle will return `undefined`.
+     * @param hypotenuse Considered optional, all constructors but the triangle will return `undefined`.
+     */
+    function Shape(name, width, height, radius, hypotenuse) {
         this.name = name;
         this.uuid = this.createUUID();
         this.width = width;
         this.height = height;
         this.radius = radius;
+        this.hypotenuse = hypotenuse;
     }
+    /**
+     * @returns Returns a base36 string of `$randomSeed_$timestamp`.
+     *
+     * Though unneccessary in this project, and technically not a functional component,
+     * it is attached to the corresponding shape as the ID on the div. The only time which
+     * it is used is when shape attributes are being displayed in the side panel.
+     */
     Shape.prototype.createUUID = function () {
         return Math.random().toString(36).substr(2, 16) + "_" + Date.now().toString(36);
     };
+    /**
+     * @returns Returns the area of a generic Shape object.
+     */
     Shape.prototype.area = function () {
         return (this.width * this.height);
     };
+    /**
+     * @returns Returns the perimeter of a generic Shape object.
+     */
     Shape.prototype.perimeter = function () {
         return ((this.width * 2) + (this.height * 2));
     };
+    /**
+     * Updates the side panel with the contextually current shape's attributes.
+     * Called by the single-click event listener.
+     */
     Shape.prototype.describeShape = function () {
         document.querySelector('#infoShapeName').value = this.name;
         document.querySelector('#infoShapeID').value = this.uuid;
@@ -37,17 +66,20 @@ var Shape = /** @class */ (function () {
         document.querySelector('#infoPerimeter').value = this.perimeter().toLocaleString() + " px";
         if (this.name === 'Triangle') {
             document.querySelector('#radiusLabel').textContent = "Hypotenuse";
-            var hypotenuse = (this.height * Math.SQRT2);
-            document.querySelector('#infoRadius').value = Math.round(hypotenuse) + " px";
+            document.querySelector('#infoRadius').value = Math.round(this.hypotenuse) + " px";
         }
         else {
             document.querySelector('#radiusLabel').textContent = "Radius";
             document.querySelector('#infoRadius').value = (this.radius) ? this.radius + " px" : '4 sides can\'t make a circle :(';
         }
     };
+    /**
+     * Creates the div of the shape drawing itself, assigns the corresponding classes and styles,
+     * adds it to the DOM, and assigns the two click listeners to describe the shape upon a single-click,
+     * and remove the shape upon a double-click.
+     */
     Shape.prototype.drawShapeElement = function () {
         var _this = this;
-        console.log(this);
         this.div = document.createElement('div');
         this.div.id = "" + this.uuid;
         this.div.className = "shape " + this.name.toLowerCase();
@@ -65,27 +97,55 @@ var Shape = /** @class */ (function () {
         this.div.addEventListener('click', function () { return _this.describeShape(); });
         this.div.addEventListener('dblclick', function () { return _this.removeShape(); });
     };
+    /**
+     * Removes the contextually current shape.
+     * Called by the double-click event listener.
+     */
     Shape.prototype.removeShape = function () {
         this.div.parentElement.removeChild(this.div);
     };
     return Shape;
 }());
+/**
+ * Definition for the inherited Circle class.
+ */
 var Circle = /** @class */ (function (_super) {
     __extends(Circle, _super);
+    /**
+     * Constructs a circle-specific shape.
+     * @param width Inferred as being twice the width of the radius.
+     * @param height Inferred as being twice the height of the radius.
+     * @param radius The value which is parsed from the Circle input.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
     function Circle(width, height, radius) {
-        var _this = _super.call(this, 'Circle', width, height, radius) || this;
+        var _this = _super.call(this, 'Circle', width, height, radius, undefined) || this;
         _this.radius = parseFloat(document.querySelector('#inputCircleRadius').value);
         _this.width = (_this.radius * 2);
         _this.height = (_this.radius * 2);
         _this.validateShapeDimensions();
         return _this;
     }
+    /**
+     * @returns Overrides the parent class' area function with the circle-specific area.
+     */
     Circle.prototype.area = function () {
         return (Math.floor((Math.PI * (Math.pow(this.radius, 2)))));
     };
+    /**
+     * @returns Overrides the parent class' area function with the circle-specific perimeter.
+     */
     Circle.prototype.perimeter = function () {
         return (Math.floor((Math.PI * this.radius * 2)));
     };
+    /**
+     * Ensures that the radius is less than half the width of the canvas. If found to be in excess of
+     * that, it will reassign the radius to the modulus of the input, then reassign the height and
+     * width to be twice the size of the new radius.
+     *
+     * An error message will be displayed to the user notifying them of the change to their circle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     Circle.prototype.validateShapeDimensions = function () {
         if (this.radius > 295) {
             this.radius %= 295;
@@ -102,25 +162,52 @@ var Circle = /** @class */ (function (_super) {
     };
     return Circle;
 }(Shape));
+/**
+ * Definition for the inherited Triangle class.
+ * This triangle generated is a right isosceles triangle.
+ */
 var Triangle = /** @class */ (function (_super) {
     __extends(Triangle, _super);
-    function Triangle(height, width) {
-        var _this = _super.call(this, 'Triangle', width, height, undefined) || this;
+    /**
+     * Constructs a right-angle isosceles triangle.
+     * @param height Inferred as equal to the width.
+     * @param width The value parsed from the Triangle input.
+     * @param hypotenuse Calculated as one of the sides times the `sqrt(2)`.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
+    function Triangle(height, width, hypotenuse) {
+        var _this = _super.call(this, 'Triangle', width, height, undefined, hypotenuse) || this;
         _this.width = parseFloat(document.querySelector('#inputIsoscelesTriangleHeight').value);
         _this.height = _this.width;
+        _this.hypotenuse = (_this.height * Math.SQRT2);
         _this.validateShapeDimensions();
         return _this;
     }
+    /**
+     * @returns Overrides the parent class' area function with the isoceles triange-specific area.
+     */
     Triangle.prototype.area = function () {
         return (Math.floor((Math.pow(this.height, 2)) / 2));
     };
+    /**
+     * @returns Overrides the parent class' area function with the isoceles triange-specific perimeter.
+     */
     Triangle.prototype.perimeter = function () {
         return (Math.floor((2 * this.height) + (Math.SQRT2 * this.height)));
     };
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input, then reassign the hypotenuse
+     * based off the newest figures.
+     *
+     * An error message will be displayed to the user notifying them of the change to their triangle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     Triangle.prototype.validateShapeDimensions = function () {
         if (this.height > 580) {
             this.height %= 580;
             this.width = this.height;
+            this.hypotenuse = (this.height * Math.SQRT2);
             Swal.fire({
                 icon: 'error',
                 title: "Tri-again!",
@@ -132,15 +219,31 @@ var Triangle = /** @class */ (function (_super) {
     };
     return Triangle;
 }(Shape));
+/**
+ * Definition for the inherited Rectangle class.
+ */
 var Rectangle = /** @class */ (function (_super) {
     __extends(Rectangle, _super);
+    /**
+     * Constructs a rectangle object.
+     * @param width Parsed from the user's input in the Rectangle width box.
+     * @param height Parsed from the user's input in the Rectangle height box.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
     function Rectangle(width, height) {
-        var _this = _super.call(this, 'Rectangle', width, height, undefined) || this;
+        var _this = _super.call(this, 'Rectangle', width, height, undefined, undefined) || this;
         _this.width = parseFloat(document.querySelector('#inputRectangleWidth').value);
         _this.height = parseFloat(document.querySelector('#inputRectangleHeight').value);
         _this.validateShapeDimensions();
         return _this;
     }
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input.
+     *
+     * An error message will be displayed to the user notifying them of the change to their rectangle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     Rectangle.prototype.validateShapeDimensions = function () {
         if (this.width > 580 || this.height > 580) {
             this.width %= 580;
@@ -156,15 +259,31 @@ var Rectangle = /** @class */ (function (_super) {
     };
     return Rectangle;
 }(Shape));
+/**
+ * Definition for the inherited Rectangle class.
+ */
 var Square = /** @class */ (function (_super) {
     __extends(Square, _super);
+    /**
+     * Constructs a square object.
+     * @param width Parsed from the user's input in the Square side length box.
+     * @param height Inferred to be equal to the width.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
     function Square(width, height) {
-        var _this = _super.call(this, 'Square', width, height, undefined) || this;
+        var _this = _super.call(this, 'Square', width, height, undefined, undefined) || this;
         _this.width = parseFloat(document.querySelector('#inputSquareSideLength').value);
         _this.height = _this.width;
         _this.validateShapeDimensions();
         return _this;
     }
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input.
+     *
+     * An error message will be displayed to the user notifying them of the change to their square's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     Square.prototype.validateShapeDimensions = function () {
         if (this.width > 580) {
             this.width %= 580;
@@ -180,7 +299,13 @@ var Square = /** @class */ (function (_super) {
     };
     return Square;
 }(Shape));
+/**
+ * Assigning event listeners to each of the 4 buttons to instantiate a new object of their corresponding
+ * shape. The new objects are sent with parameters of 0, since the constructor will always assign the
+ * required values based off what it pulls from the inputs. This was simply done to keep this concise,
+ * to avoid what should be class-based functional logic from being created outside the class.
+ */
 document.querySelector('#addRectangleButton').addEventListener('click', function () { return new Rectangle(0, 0); });
 document.querySelector('#addSquareButton').addEventListener('click', function () { return new Square(0, 0); });
-document.querySelector('#addCircleButton').addEventListener('click', function () { return new Circle(0, 1, 2); });
-document.querySelector('#addIsocelesButton').addEventListener('click', function () { return new Triangle(0, 0); });
+document.querySelector('#addCircleButton').addEventListener('click', function () { return new Circle(0, 0, 0); });
+document.querySelector('#addIsocelesButton').addEventListener('click', function () { return new Triangle(0, 0, 0); });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-declare let Swal: any;
-document.querySelector('#addRectangleButton').addEventListener('click', () =>  new Rectangle(0,0));
-document.querySelector('#addSquareButton').addEventListener('click', () =>  new Square(0,0));
-document.querySelector('#addCircleButton').addEventListener('click', () => new Circle(0,1,2));
-document.querySelector('#addIsocelesButton').addEventListener('click', () =>  new Triangle(0,0));
-
+/**
+ * Definitions of the generic parent shape class.
+ */
 class Shape {
     name: string;
     div: HTMLElement;
@@ -11,27 +8,54 @@ class Shape {
     width: number;
     height: number;
     radius: number;
+    hypotenuse: number;
 
-    constructor(name: string, width?: number, height?: number, radius?: number) {
+    /**
+     * Assigns generic Shape attributes.
+     * @param name Only required parameter. Supplied in the constructor of each child constructor.
+     * @param width Considered optional, though all current shapes will return this (including circles).
+     * @param height Considered optional, though all current shapes will return this (including circles).
+     * @param radius Considered optional, all constructors but the circle will return `undefined`.
+     * @param hypotenuse Considered optional, all constructors but the triangle will return `undefined`.
+     */
+    constructor(name: string, width?: number, height?: number, radius?: number, hypotenuse?: number) {
         this.name = name;
         this.uuid = this.createUUID();
         this.width = width;
         this.height = height;
         this.radius = radius;
+        this.hypotenuse = hypotenuse;
     }
 
+    /**
+     * @returns Returns a base36 string of `$randomSeed_$timestamp`.
+     * 
+     * Though unneccessary in this project, and technically not a functional component,
+     * it is attached to the corresponding shape as the ID on the div. The only time which
+     * it is used is when shape attributes are being displayed in the side panel.
+     */
     createUUID(): string {
         return `${Math.random().toString(36).substr(2, 16)}_${Date.now().toString(36)}`;
     }
 
+    /**
+     * @returns Returns the area of a generic Shape object.
+     */
     area(): number {
         return (this.width * this.height);
     }
 
+    /**
+     * @returns Returns the perimeter of a generic Shape object.
+     */
     perimeter(): number {
         return ((this.width * 2) + (this.height * 2));
     }
 
+    /**
+     * Updates the side panel with the contextually current shape's attributes.
+     * Called by the single-click event listener.
+     */
     describeShape() {
         (<HTMLInputElement>document.querySelector('#infoShapeName')).value = this.name;
         (<HTMLInputElement>document.querySelector('#infoShapeID')).value = this.uuid;
@@ -41,14 +65,18 @@ class Shape {
         (<HTMLInputElement>document.querySelector('#infoPerimeter')).value = `${this.perimeter().toLocaleString()} px`;
         if (this.name === 'Triangle') {
             document.querySelector('#radiusLabel').textContent = "Hypotenuse";
-            let hypotenuse: number = (this.height * Math.SQRT2);
-            (<HTMLInputElement>document.querySelector('#infoRadius')).value = `${Math.round(hypotenuse)} px`;
+            (<HTMLInputElement>document.querySelector('#infoRadius')).value = `${Math.round(this.hypotenuse)} px`;
         } else {
             document.querySelector('#radiusLabel').textContent = "Radius";
             (<HTMLInputElement>document.querySelector('#infoRadius')).value = (this.radius) ? `${this.radius} px` : '4 sides can\'t make a circle :(';
         }
     }
 
+    /**
+     * Creates the div of the shape drawing itself, assigns the corresponding classes and styles,
+     * adds it to the DOM, and assigns the two click listeners to describe the shape upon a single-click,
+     * and remove the shape upon a double-click.
+     */
     drawShapeElement() {
         this.div = document.createElement('div');
         this.div.id = `${this.uuid}`;
@@ -69,28 +97,56 @@ class Shape {
         this.div.addEventListener('dblclick', () => this.removeShape());
     }
 
+    /**
+     * Removes the contextually current shape.
+     * Called by the double-click event listener.
+     */
     removeShape() {
         this.div.parentElement.removeChild(this.div);
     }
 }
 
+/**
+ * Definition for the inherited Circle class.
+ */
 class Circle extends Shape {
+    /**
+     * Constructs a circle-specific shape.
+     * @param width Inferred as being twice the width of the radius.
+     * @param height Inferred as being twice the height of the radius.
+     * @param radius The value which is parsed from the Circle input.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
      constructor(width: number, height: number, radius: number) {
-        super('Circle', width, height, radius);
+        super('Circle', width, height, radius, undefined);
         this.radius = parseFloat((<HTMLInputElement>document.querySelector('#inputCircleRadius')).value);
         this.width = (this.radius * 2);
         this.height = (this.radius * 2);
         this.validateShapeDimensions();
     }
 
+    /**
+     * @returns Overrides the parent class' area function with the circle-specific area.
+     */
     area(): number {
         return (Math.floor((Math.PI * (this.radius ** 2))));
     }
     
+    /**
+     * @returns Overrides the parent class' area function with the circle-specific perimeter.
+     */
     perimeter(): number {
         return (Math.floor((Math.PI * this.radius * 2)));
     }
 
+    /**
+     * Ensures that the radius is less than half the width of the canvas. If found to be in excess of
+     * that, it will reassign the radius to the modulus of the input, then reassign the height and 
+     * width to be twice the size of the new radius.
+     * 
+     * An error message will be displayed to the user notifying them of the change to their circle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     validateShapeDimensions() {
         if (this.radius > 295) {
             this.radius %= 295;
@@ -107,26 +163,55 @@ class Circle extends Shape {
     }
 }
 
+/**
+ * Definition for the inherited Triangle class.
+ * This triangle generated is a right isosceles triangle.
+ */
 class Triangle extends Shape {
-    constructor(height: number, width: number) {
-        super('Triangle', width, height, undefined);
+    hypotenuse: number;
+
+    /**
+     * Constructs a right-angle isosceles triangle.
+     * @param height Inferred as equal to the width.
+     * @param width The value parsed from the Triangle input. 
+     * @param hypotenuse Calculated as one of the sides times the `sqrt(2)`.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
+    constructor(height: number, width: number, hypotenuse: number) {
+        super('Triangle', width, height, undefined, hypotenuse);
         this.width = parseFloat((<HTMLInputElement>document.querySelector('#inputIsoscelesTriangleHeight')).value);
         this.height = this.width;
+        this.hypotenuse = (this.height * Math.SQRT2);
         this.validateShapeDimensions();
     }
 
+    /**
+     * @returns Overrides the parent class' area function with the isoceles triange-specific area.
+     */
     area(): number {
         return (Math.floor((this.height ** 2) / 2));
     }
 
+    /**
+     * @returns Overrides the parent class' area function with the isoceles triange-specific perimeter.
+     */
     perimeter(): number {
         return (Math.floor((2 * this.height) + (Math.SQRT2 * this.height)));
     }
 
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input, then reassign the hypotenuse
+     * based off the newest figures.
+     * 
+     * An error message will be displayed to the user notifying them of the change to their triangle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     validateShapeDimensions() {
         if (this.height > 580) {
             this.height %= 580;
             this.width = this.height;
+            this.hypotenuse = (this.height * Math.SQRT2);
             Swal.fire({
                 icon: 'error',
                 title: "Tri-again!",
@@ -138,14 +223,30 @@ class Triangle extends Shape {
     }
 }
 
+/**
+ * Definition for the inherited Rectangle class.
+ */
 class Rectangle extends Shape {
+    /**
+     * Constructs a rectangle object.
+     * @param width Parsed from the user's input in the Rectangle width box.
+     * @param height Parsed from the user's input in the Rectangle height box.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
     constructor(width: number, height: number) {
-        super('Rectangle', width, height, undefined);
+        super('Rectangle', width, height, undefined, undefined);
         this.width = parseFloat((<HTMLInputElement>document.querySelector('#inputRectangleWidth')).value);
         this.height = parseFloat((<HTMLInputElement>document.querySelector('#inputRectangleHeight')).value);
         this.validateShapeDimensions();
     }
 
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input.
+     * 
+     * An error message will be displayed to the user notifying them of the change to their rectangle's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     validateShapeDimensions() {
         if (this.width > 580 || this.height > 580) {
             this.width %= 580;
@@ -161,14 +262,30 @@ class Rectangle extends Shape {
     }
 }
 
+/**
+ * Definition for the inherited Rectangle class.
+ */
 class Square extends Shape {
+    /**
+     * Constructs a square object.
+     * @param width Parsed from the user's input in the Square side length box.
+     * @param height Inferred to be equal to the width.
+     * Sends values to the validator function, which ensures nothing extends past the bounds of the canvas.
+     */
     constructor(width: number, height: number) {
-        super('Square', width, height, undefined);
+        super('Square', width, height, undefined, undefined);
         this.width = parseFloat((<HTMLInputElement>document.querySelector('#inputSquareSideLength')).value);
         this.height = this.width;
         this.validateShapeDimensions();
     }
 
+    /**
+     * Ensures that the height & width are less than that of the canvas'. If found to be in excess of
+     * that, it will reassign the height & width to the modulus of the input.
+     * 
+     * An error message will be displayed to the user notifying them of the change to their square's
+     * dimensions, then it will call the parent `drawShapeElement()` function.
+     */
     validateShapeDimensions() {
         if (this.width > 580) {
             this.width %= 580;
@@ -183,3 +300,19 @@ class Square extends Shape {
         this.drawShapeElement();
     }
 }
+
+/**
+ * Declaring SweetAlerts as a global variable with a type of any, to help suppress TS warnings since SweetAlerts doesn't have an implicit type
+ */
+declare let Swal: any;
+
+/**
+ * Assigning event listeners to each of the 4 buttons to instantiate a new object of their corresponding
+ * shape. The new objects are sent with parameters of 0, since the constructor will always assign the
+ * required values based off what it pulls from the inputs. This was simply done to keep this concise,
+ * to avoid what should be class-based functional logic from being created outside the class.
+ */
+document.querySelector('#addRectangleButton').addEventListener('click', () =>  new Rectangle(0,0));
+document.querySelector('#addSquareButton').addEventListener('click', () =>  new Square(0,0));
+document.querySelector('#addCircleButton').addEventListener('click', () => new Circle(0,0,0));
+document.querySelector('#addIsocelesButton').addEventListener('click', () =>  new Triangle(0,0,0));
